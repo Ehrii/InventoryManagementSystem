0d092c97a7760b9b13ec790a40edbdc9d1802328
John Eriel <93180808+Ehrii@users.noreply.github.com>
2025-02-05 10:25:07 +0800

    Added an Inventory Manger Class

--- InventoryManagementSystem/Program.cs
+++ InventoryManagementSystem/Program.cs
@@ -4,14 +4,11 @@ namespace InventoryManagementSystem
 {
     public class Program
     {
-        private static List<Product>products = new List<Product>();
-
         static void Main(string[] args)
         {
             Program program = new Program();
             bool exit = true;
             while (exit!=false) {
-
                 Console.WriteLine("----------------------------------------------------");
                 Console.WriteLine("Inventory Management System");
                 Console.WriteLine("----------------------------------------------------");
@@ -29,13 +26,13 @@ namespace InventoryManagementSystem
                 switch (userChoice)
                 {
                     case "1":
-                        program.addProduct(createProduct());
+                        InventoryManager.addProduct(InventoryManager.createProduct());
                         break;
                     case "2":
                         Console.WriteLine("Enter a Product ID to remove");
                         if (int.TryParse(Console.ReadLine(), out int productId)) 
                         {
-                            program.removeProduct(productId);
+                         InventoryManager.removeProduct(productId);
                         }
                         else
                         {
@@ -49,7 +46,7 @@ namespace InventoryManagementSystem
                             Console.WriteLine("Enter Quantity to Update: ");
                             if (int.TryParse(Console.ReadLine(), out int newQuantity) && newQuantity > 0)
                             {
-                               program.updateProduct(updateId,newQuantity);
+                               InventoryManager.updateProduct(updateId,newQuantity);
                             }
                         }
                         else
@@ -58,118 +55,20 @@ namespace InventoryManagementSystem
                         }
                         break;
                     case "4":
-                        program.viewProduct();
+                        InventoryManager.viewProduct();
                         break;
                     case "5":
-                        program.getTotalValue();
+                        InventoryManager.getTotalValue();
                         break;
-                    case "X":
+                    case "X": 
                     case "x":
                         exit = false;
                         break;
                     default:
                         Console.WriteLine("Please Input a Valid Choice, Try Again!");
                         break;
-                       
                 }
             }
-        }
-
-        void addProduct(Product product)
-        {
-            products.Add(product);
-            Console.WriteLine($"Product {product.prodName} added successfully");
-        }
-
-        static Product createProduct()
-        {
-            while (true)
-            {
-                Console.WriteLine("Enter a Product Name to Add (Ex.Apple) : ");
-                string? prodName = Console.ReadLine();
-
-                while (string.IsNullOrEmpty(prodName) || prodName.Any(char.IsDigit))
-                {
-                    Console.WriteLine("Please try again, Enter a Product Name to Add (Ex.Apple) : ");
-                    prodName = Console.ReadLine();
-                }
-             
-                Console.WriteLine("Enter Product Quantity (Ex.2) : ");
-                if (int.TryParse(Console.ReadLine(), out int quantity) && quantity>0)
-                {
-                    Console.WriteLine("Enter Product Price (Ex. 50) : ");
-                    if (int.TryParse(Console.ReadLine(), out int prodPrice) && prodPrice>0)
-                    {
-                        Product newProduct = new Product(products.Count + 1, prodName, quantity, prodPrice);
-                        return newProduct;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid Product Price, Please Enter a Valid Price");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Quantity, Please Enter a Valid Integer");
-                }
-            }
-        }
-
-       void removeProduct(int productId)
-        {
-            Product ?productToRemove = products.Find(p => p.prodId == productId);
-
-            if (productToRemove != null)
-            {
-                products.Remove(productToRemove);
-                Console.WriteLine($"Product with {productId} removed.");
-            }
-            else
-            {
-                Console.WriteLine($"No Product found with ID {productId}.");
-            }
-        }
-
-        void updateProduct(int productId, int newQuantity)
-        {
-            Product? producToUpdate = products.Find(p=> p.prodId==productId);
-            if (producToUpdate != null)
-            {
-                producToUpdate.prodQuantity = newQuantity;
-                Console.WriteLine($"Product ID {productId} updated with new quantity: {newQuantity}");
-            }
-            else
-            {
-                Console.WriteLine($"No product found with ID {productId}.");
-            }
-        }
-
-        void viewProduct()
-        {
-            if (products.Count > 0)
-            {
-                Console.WriteLine("Product List");
-                foreach (Product product in products)
-                {
-                    Console.WriteLine($"ID: {product.prodId}, Name: {product.prodName}, Quantity: {product.prodQuantity}, Price: {product.prodPrice:N2} Pesos");
-                    Console.WriteLine("----------------------------------------------------");
-                    Console.WriteLine();
-                }
-            }
-            else
-            {
-                Console.WriteLine("No Products Available, Please add one!");
-            }
-        }
-
-        void getTotalValue()
-        {
-            double totalValue = 0.00;
-            foreach (var product in products)
-            {
-                totalValue += product.prodQuantity * product.prodPrice;
-            }
-            Console.WriteLine($"Total Value of Products: {totalValue:N2} Pesos");
         }
     }
 
